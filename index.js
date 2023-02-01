45869e08fe2c2172e711bcb9a8a0365d6dedf0b6
Asahan Sahan <asahansahan@Asahans-MacBook-Pro-2.local>
2023-02-01 14:50:21 -0500

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,4 +21,8 @@ app.use('/', (req, res, next) => {
 });
 
 
-app.listen(3000);
+
+app.listen(port, (req, res, next) => {
+    console.log('server is started on port 3000');
+});
+
